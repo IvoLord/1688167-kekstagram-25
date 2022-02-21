34368fde921d6b13ac30431afeb2a17dc0b00c11
IvoLord <oleksandr.kotovskyi@gmail.com>
2022-02-21 23:54:32 +0100

    update dz

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-// рандомное число между или равно значениям
-function randomNumberBetween(min, max) {
-  return Math.round(Math.random() * (max - min)) + min;
-}
+// рандомное число между или равно значениям.
+const RANDOM_NUMBER_BETWEEN = (min, max) => {
+  const MIN = (min < 0) ? min = 0 : min;
+  const MAX = (max < min) ? max = min : max;
+  return Math.round(Math.random() * (MAX - MIN)) + MIN;
+};
 
-randomNumberBetween(0, 100);
+RANDOM_NUMBER_BETWEEN(-2, 3);
 
 // проверка длинны коментария, проверку проходит то TRUE
-function stringLenth(string, maxLenghtString) {
-  if (string.length <= maxLenghtString) {
-    return true;
-  }
+const STRING_LENGHT = (string, maxLenghtString) => {
+  const COMPARISON = string.length <= maxLenghtString;
+  return COMPARISON;
+};
 
-  return false;
-}
-
-stringLenth('привет dfsdgsd gdsf g sdfhdfghf gj fdg j gfh jgf hjgf hj g', 140);
+STRING_LENGHT('привет', 140);
